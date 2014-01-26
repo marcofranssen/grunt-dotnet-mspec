b39d65cc48121a64bfd63d43fbaf79a2e6bbe067
Marco Franssen <marco.franssen@gmail.com>
2014-01-26 14:37:39 +0100

    included new test files and removed the example test files

---
+++ test/src/MySpecs/MySpecs.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using Machine.Specifications;
+
+namespace MySpecs
+{
+    [Subject("Awesomeness using node.js grunt test runner"), Tags("nodejs", "grunt")]
+    public class When_I_am_executed_by_the_grunt_task
+    {
+        Because of = () => Exception = Catch.Exception(() => Value = true);
+
+        It should_have_set_the_value_to_true = () => Value.ShouldBeTrue();
+        It should_not_throw_an_exception = () => Exception.ShouldBeNull();
+
+        private static bool Value;
+        private static Exception Exception;
+    }
+}
