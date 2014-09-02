497b1c4ea05f089597d833430027964a2c65e624
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-09-02 14:28:42 +0200

    Describing two specs testing the parameter handling

---
+++ test/mspec.specs.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var mspec = require('../tasks/mspec.js')
+
+describe('MSpec parameter handling', function(){
+  before(function(){
+
+  })
+  describe('If ONE *.dll is passed to grunt-dotnet-mspec', function(){
+    it('should grep this ONE', function(){
+
+    });
+  });
+
+  describe('If TWO *.dll is passed to grunt-dotnet-mspec', function(){
+    it('should grep this TWO', function(){
+
+    });
+  });
+});
