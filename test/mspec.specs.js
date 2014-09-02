8535b43e0d8d7888db8548664c77afc8cb193a5a
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-09-02 22:39:06 +0200

    Arranging specification

--- test/mspec.specs.js
+++ test/mspec.specs.js
@@ -1,13 +1,17 @@
-var assert = require('assert');
-var mspec = require('../tasks/mspec.js')
+var assert = require('assert'),
+    grunt = require('grunt'),
+    mspec = require('../tasks/mspec.js');
 
 describe('MSpec parameter handling', function(){
   before(function(){
+    var options = {};
+    var oneSpecFile = { 'Spec.dll' };
+    var twoSpecFiles = { 'Specs.dll', 'SpecsTwo.dll' };
+  });
 
-  })
   describe('If ONE *.dll is passed to grunt-dotnet-mspec', function(){
     it('should grep this ONE', function(){
-
+      mspec.buildCommand();
     });
   });
 
