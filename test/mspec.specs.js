81d58fd65acd38b11a0d6e2486d10269890421f4
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-09-03 14:18:15 +0200

    Fixing arrangement of files-object

--- test/mspec.specs.js
+++ test/mspec.specs.js
@@ -2,23 +2,28 @@ var assert = require('assert'),
     grunt = require('grunt'),
     Command = require('../tasks/mspec.command');
 
-    var options = {};
-    var oneSpecFile = { src: 'Spec.dll' };
+    var options = { platform: 'anycpu' };
+    var oneSpecFile = [{ src: ['Spec.dll'] }];
+    var twoSpecFiles = [{ src: ['SpecOne.dll' , 'SpecTwo.dll'] }];
 
-describe('MSpec parameter handling', function(){
+describe('MSpec parameter handling', function() {
   
   describe('If ONE *.dll is passed to grunt-dotnet-mspec', function(){
 
-    it('should grep this ONE', function(){
+    it('should grep this ONE', function() {
       var command = new Command(grunt, oneSpecFile, options);
-      assert.equal(command.args[0], 'Spec.dll');
+      assert.equal(command.args[0], '"Spec.dll"');
     });
   });
 
   describe('If TWO *.dlls are passed to grunt-dotnet-mspec', function(){
 
     it('should grep these TWO', function(){
-
+      var command = new Command(grunt, twoSpecFiles, options);
+      
+      assert.equal(command.args.length, 2);
+      assert.equal(command.args[0], '"SpecOne.dll"');
+      assert.equal(command.args[1], '"SpecTwo.dll"');
     });
   });
 });
