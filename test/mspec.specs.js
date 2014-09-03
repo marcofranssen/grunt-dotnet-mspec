aec0b67426b057ce522d736c665f5e89ee8a91f7
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-09-03 13:17:26 +0200

    Replacing mspec with mspec.command to test creation of command

--- test/mspec.specs.js
+++ test/mspec.specs.js
@@ -1,19 +1,22 @@
 var assert = require('assert'),
     grunt = require('grunt'),
-    mspec = require('../tasks/mspec.js');
+    Command = require('../tasks/mspec.command');
 
     var options = {};
     var oneSpecFile = { src: 'Spec.dll' };
 
 describe('MSpec parameter handling', function(){
+  
   describe('If ONE *.dll is passed to grunt-dotnet-mspec', function(){
+
     it('should grep this ONE', function(){
-      var command = mspec.buildCommand(grunt, oneSpecFile, options);
+      var command = new Command(grunt, oneSpecFile, options);
       assert.equal(command.args[0], 'Spec.dll');
     });
   });
 
   describe('If TWO *.dlls are passed to grunt-dotnet-mspec', function(){
+
     it('should grep these TWO', function(){
 
     });
