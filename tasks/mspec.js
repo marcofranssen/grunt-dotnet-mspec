de4edc2335624535e53816e6843de2cc85c1c7c9
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-01-02 14:24:00 +0100

    Readding log- & command-module and meta information (name, description)

--- tasks/mspec.js
+++ tasks/mspec.js
@@ -8,44 +8,49 @@
 
 'use strict';
 
-var path = require('path'),
-  buildCommand = function(grunt, files, options) {
-    var mspec = options.platform === 'x86' ? 'mspec-x86-clr4.exe' : 'mspec-clr4.exe';
-    if (options.toolsPath) {
-      if (!grunt.file.isPathAbsolute(options.toolsPath)) {
-        options.toolsPath = path.join(process.cwd(), options.toolsPath);
+var path          = require('path'),
+    log           = require('../lib/mspec.log'),
+    Command       = require('../lib/mspec.command'),
+    name          = 'mspec',
+    description   = 'Grunt plugin to run Machine.Specfication tests',
+    buildCommand  = function(grunt, files, options) {
+
+      var mspec = options.platform === 'x86' ? 'mspec-x86-clr4.exe' : 'mspec-clr4.exe';
+      if (options.toolsPath) {
+        if (!grunt.file.isPathAbsolute(options.toolsPath)) {
+          options.toolsPath = path.join(process.cwd(), options.toolsPath);
+        }
+        mspec = path.join(options.toolsPath, mspec);
       }
-      mspec = path.join(options.toolsPath, mspec);
-    }
-    mspec = mspec.replace(/\\/g, path.sep);
-    var assemblies = files.map(function(file) {
-      return file.src;
-    });
-    var args = assemblies;
+      mspec = mspec.replace(/\\/g, path.sep);
+      var assemblies = files.map(function(file) {
+        return file.src;
+      });
+      var args = assemblies;
 
-    if (options.timeinfo) {
-      args.unshift('-t');
-    }
-    if (options.silent) {
-      args.unshift('-s');
-    }
-    if (options.progress) {
-      args.unshift('-p');
-    }
-    if (options.output) {
-      var filePath = path.join(process.cwd(), options.output);
-      grunt.file.mkdir(filePath);
-      args.unshift(path.join(filePath, 'index.xml'));
-      args.unshift('--xml');
-      args.unshift(filePath);
-      args.unshift('--html');
-    }
+      if (options.timeinfo) {
+        args.unshift('-t');
+      }
+      if (options.silent) {
+        args.unshift('-s');
+      }
+      if (options.progress) {
+        args.unshift('-p');
+      }
+      if (options.output) {
+        var filePath = path.join(process.cwd(), options.output);
+        grunt.file.mkdir(filePath);
+        args.unshift(path.join(filePath, 'index.xml'));
+        args.unshift('--xml');
+        args.unshift(filePath);
+        args.unshift('--html');
+      }
 
-    return {
-      path: path.normalize(mspec),
-      args: args
+      return {
+        path: path.normalize(mspec),
+        args: args
+      };
     };
-  };
 
 module.exports = function(grunt) {
 
